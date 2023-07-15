e390174e079496c0ff2400672c538a83ba658c15
niranjana <niranjana@qburst.com>
2023-07-15 23:02:22 +0530

    Added predict functionality

---
+++ predict_location.py
@@ -0,0 +1,23 @@
+from get_data.bluetooth_devices import get_bluetooth_scanner
+from get_data.wifi_devices import get_wifi_scanner
+import pickle
+
+
+def predict():
+    wifi_scanner = get_wifi_scanner()
+    bluetooth_scanner = get_bluetooth_scanner()
+
+    bluetooth_devices = bluetooth_scanner.get_connected_device_and_rssi()
+    wifi_devices = wifi_scanner.get_access_points()
+
+    current_data = {**wifi_devices, **bluetooth_devices}
+
+    with open('saved_model.pkl', "rb") as f:
+        lp = pickle.load(f)
+    return lp.predict(current_data)[0]
+
+def main():
+    print(predict())
+
+if __name__ == '__main__':
+    main()
--- train_model.py
+++ train_model.py
@@ -1,6 +1,14 @@
 import os
 from get_data.utils import ensure_data_path
 import json
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.pipeline import make_pipeline
+import pickle
+
+
+def get_pipeline(clf=RandomForestClassifier(n_estimators=100, class_weight="balanced")):
+    return make_pipeline(DictVectorizer(sparse=False), clf)
 
 
 def get_train_data(folder=None):
@@ -18,8 +26,19 @@ def get_train_data(folder=None):
             y.extend([fname.rstrip(".txt")] * len(data))
     return X, y
 
+
+def train_model(path=None):
+    X, y = get_train_data(path)
+    if len(X) == 0:
+        raise ValueError("No wifi access points have been found during training")
+    lp = get_pipeline()
+    lp.fit(X, y)
+    with open('saved_model.pkl', "wb") as f:
+        pickle.dump(lp, f)
+    return lp
+
 def main():
-    get_train_data()
+    train_model()
 
 if __name__ == '__main__':
     main()
